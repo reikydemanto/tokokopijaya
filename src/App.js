da285cf714a2ac2c70d1ae9fb529ca19408533c2
Reiky De Manto <reikydemanto@gmail.com>
2023-01-11 13:59:08 +0700

    Coming soon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Welcome = (props) => {
+  return (
+    <div>
+      <h2>{props.nama}</h2>
+      <h2>{props.sekolah}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Coming Soon</h1>
       </header>
     </div>
   );
